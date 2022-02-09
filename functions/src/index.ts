222f1a4cebd1fc6904b5a748d78ed811899c16d6
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-02-09 08:00:16 +0100

    Various stuff...

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,29 @@
-//import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const BASE_URL = "https://www.strava.com/oauth";
+const SCOPE = "read,activity:read_all";
+
+export const authorize = functions.https.onRequest(
+  async (request, response) => {
+    const redirectUri = request.query.redirect_uri;
+    response.redirect(
+      `${BASE_URL}/authorize?client_id=${
+        functions.config().strava.clientid
+      }&response_type=code&redirect_uri=${redirectUri}&approval_prompt=force&scope=${SCOPE}`
+    );
+  }
+);
+
+export const token = functions.https.onRequest(async (request, response) => {
+  const url = `${BASE_URL}/token?client_id=${
+    functions.config().strava.clientid
+  }&client_secret=${functions.config().strava.clientsecret}&code=${
+    request.query.code
+  }&grant_type=authorization_code`;
+
+  const tokenResponse = await fetch(url, {
+    method: "POST",
+  });
+
+  response.send(await tokenResponse.text());
+});
