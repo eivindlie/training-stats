26ad80bd1686360f03fb539a7c7afb2f92ce610b
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-02-09 08:51:43 +0100

    Add CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import fetch from "node-fetch";
+import * as cors from "cors";
 
 const BASE_URL = "https://www.strava.com/oauth";
 const SCOPE = "read,activity:read_all";
@@ -8,37 +9,43 @@ const REDIRECT_URI =
 
 export const authorize = functions.https.onRequest(
   async (request, response) => {
-    response.redirect(
-      `${BASE_URL}/authorize?client_id=${
-        functions.config().strava.clientid
-      }&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&state=${
-        request.query.state
-      }`
-    );
+    cors()(request, response, () => {
+      response.redirect(
+        `${BASE_URL}/authorize?client_id=${
+          functions.config().strava.clientid
+        }&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&state=${
+          request.query.state
+        }`
+      );
+    });
   }
 );
 
 export const token = functions.https.onRequest(async (request, response) => {
-  const url = `${BASE_URL}/token?client_id=${
-    functions.config().strava.clientid
-  }&client_secret=${functions.config().strava.clientsecret}&code=${
-    request.query.code
-  }&grant_type=authorization_code`;
+  cors()(request, response, async () => {
+    const url = `${BASE_URL}/token?client_id=${
+      functions.config().strava.clientid
+    }&client_secret=${functions.config().strava.clientsecret}&code=${
+      request.query.code
+    }&grant_type=authorization_code`;
 
-  const tokenResponse = await fetch(url, {
-    method: "POST",
+    const tokenResponse = await fetch(url, {
+      method: "POST",
+    });
+
+    response.send(await tokenResponse.text());
   });
-
-  response.send(await tokenResponse.text());
 });
 
 export const redirect = functions.https.onRequest(async (request, response) => {
-  const state = JSON.parse(request.query.state as string);
-  let url: string;
-  if (state["environment"] === "development") {
-    url = "http://localhost:3000/oauth/callback";
-  } else {
-    url = "https://training-stats.andreassen.info/oauth/callback";
-  }
-  response.redirect(`${url}?code=${request.query.code}`);
+  cors()(request, response, () => {
+    const state = JSON.parse(request.query.state as string);
+    let url: string;
+    if (state["environment"] === "development") {
+      url = "http://localhost:3000/oauth/callback";
+    } else {
+      url = "https://training-stats.andreassen.info/oauth/callback";
+    }
+    response.redirect(`${url}?code=${request.query.code}`);
+  });
 });
